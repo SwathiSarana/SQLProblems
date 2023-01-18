e815214fe263b830288dc5e448f3bf645a93f58e
SwathiSarana <swathi11ec@gail.com>
2023-01-17 19:43:52 -0800

    all Aggregations

---
+++ 12.sql
@@ -0,0 +1,5 @@
+--Query a count of the number of cities in CITY having a Population larger than 100000.
+
+SELECT COUNT (NAME) FROM CITY
+Where POPULATION > 100000;
+
---
+++ 14.sql
@@ -0,0 +1,4 @@
+--uery the average population of all cities in CITY where District is California.
+
+SELECT AVG(POPULATION) From CITY
+Where DISTRICT = 'California';
